5635e73711cf4f45f1645c41fba10c99e1fb8a75
BETHANY FOLINO <bethany.folino@gmail.com>
2021-03-03 13:13:50 -0800

    Added link to products detail page on ProductSummary

--- src/components/ProductSummary.js
+++ src/components/ProductSummary.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ProductSummary(props) {
   return (
     <div className="productSummary">
       {props.product.name} - ${props.product.price}{" "}
       <span className="productArrow">&gt;</span>
+      <Link to={"/products/detail/" + props.product.id}>Product Detail</Link>
     </div>
   );
 }
