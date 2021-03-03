d82a5d682e5a85a2a2461658cc5c517833327ca3
BETHANY FOLINO <bethany.folino@gmail.com>
2021-03-03 13:40:16 -0800

    Made useEffect hook

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -6,14 +6,21 @@ function ProductDetail(props) {
   // When this component loads, get the Product ID from the url parameter.
   // Then find the matching product from the productList prop, and assign it to product
 
+  useEffect(() => {
+    fetch("localhost:3000/products")
+      .then((res) => res.json())
+      .then((data) => setProduct(data.message));
+    myList =
+      productList.find((item) => item.id === props.match.params.productId) ||
+      {};
+  });
+
   return (
     <div>
       <h3>{product.name}</h3>
       <div>Price: ${product.price}</div>
       <p>{product.description}</p>
-      <Link to="/products">
-        Go Back
-      </Link>
+      <Link to="/products">Go Back</Link>
     </div>
   );
 }
